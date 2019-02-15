81bcda67df43413575a14ddff2de61dd82217572
appchto <appchto@gmail.com>
2019-02-15 01:33:25 -0200

    +

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+},
+  password: {
+    type: String,
+    required: true
+  },
+  date_created: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
